2128bd4212b47a422c8c3ed704dbb236f6564ce6
neeraj31285 <neeraj.singh31285@gmail.com>
2024-06-20 07:43:05 +0530

    code formatted

--- ReflectionTemplateLib/inc/Constants.h
+++ ReflectionTemplateLib/inc/Constants.h
@@ -4,17 +4,17 @@
 
 namespace rtl {
 
-	const char* const NS_GLOBAL = "namespace_global";
-	const char* const CTOR_SUFFIX = "::ctor::";
+    const char* const NS_GLOBAL = "namespace_global";
+    const char* const CTOR_SUFFIX = "::ctor::";
 
-#define GETTER(_type, _name, _var)							\
-		inline constexpr const _type& get##_name() const {	\
-			return _var;									\
-		}
+#define GETTER(_type, _name, _var)                        \
+    inline constexpr const _type& get##_name() const {    \
+        return _var;                                      \
+    }
 
-	template<class _typeA, class _typeB>
-	using enable_if_same = typename std::enable_if< std::is_same<_typeA, _typeB>::value >::type;
+    template<class _typeA, class _typeB>
+    using enable_if_same = typename std::enable_if< std::is_same<_typeA, _typeB>::value >::type;
 
-	template<class _typeA, class _typeB>
-	using enable_if_notSame = typename std::enable_if< !std::is_same<_typeA, _typeB>::value >::type;
+    template<class _typeA, class _typeB>
+    using enable_if_notSame = typename std::enable_if< !std::is_same<_typeA, _typeB>::value >::type;
 }
