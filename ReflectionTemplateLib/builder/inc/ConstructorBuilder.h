2bae2251f0eda4d594cee3b54582cb4465bdb9b9
neeraj31285 <neeraj.singh31285@gmail.com>
2024-07-18 20:01:23 +0530

    replaced SFINAE with if constexpr(), cleaner approach

--- ReflectionTemplateLib/builder/inc/ConstructorBuilder.h
+++ ReflectionTemplateLib/builder/inc/ConstructorBuilder.h
@@ -6,7 +6,7 @@ namespace rtl {
 
 	namespace builder 
 	{
-		template<class _recordType = void, class ..._ctorSignature>
+		template<class _recordType, class ..._ctorSignature>
 		class ConstructorBuilder
 		{
 			const std::string& m_record;
@@ -21,34 +21,6 @@ namespace rtl {
 					   const FunctorType& pCtorType);
 
 			inline constexpr const access::Function build() const;
-		};
-	}
-
-
-	namespace builder
-	{
-		template<>
-		class ConstructorBuilder<>
-		{
-		public:
-
-			template<class _recordType, class ..._signature>
-			static constexpr const ConstructorBuilder<_recordType, _signature...>
-				select(const std::string& pNamespace, const std::string& pRecord,
-					enable_if_same<_recordType&, typename detail::TypeId<_signature...>::HEAD > *_= nullptr);
-
-
-			template<class _recordType, class ..._signature>
-			static constexpr const ConstructorBuilder<_recordType, _signature...>
-				select(const std::string& pNamespace, const std::string& pRecord,
-					enable_if_same<const _recordType&, typename detail::TypeId<_signature...>::HEAD > *_= nullptr);
-
-
-			template<class _recordType, class ..._signature>
-			static constexpr const ConstructorBuilder<_recordType, _signature...>
-				select(const std::string& pNamespace, const std::string& pRecord,
-					enable_if_not_same<_recordType&, typename detail::TypeId<_signature...>::HEAD > *_= nullptr,
-					enable_if_not_same<const _recordType&, typename detail::TypeId<_signature...>::HEAD > *__= nullptr);
 		};
 	}
 }
--- ReflectionTemplateLib/builder/inc/ConstructorBuilder.hpp
+++ ReflectionTemplateLib/builder/inc/ConstructorBuilder.hpp
@@ -39,35 +39,4 @@ namespace rtl {
 			}
 		}
 	}
-
-
-	namespace builder
-	{
-		template<class _recordType, class ..._signature>
-		inline constexpr const ConstructorBuilder<_recordType, _signature...>
-			ConstructorBuilder<>::select(const std::string& pNamespace, const std::string& pRecord,
-				enable_if_same<_recordType&, typename detail::TypeId<_signature...>::HEAD > *_)
-		{
-			return ConstructorBuilder<_recordType, _signature...>(pNamespace, pRecord, FunctorType::CopyCtor);
-		}
-
-
-		template<class _recordType, class ..._signature>
-		inline constexpr const ConstructorBuilder<_recordType, _signature...>
-			ConstructorBuilder<>::select(const std::string& pNamespace, const std::string& pRecord,
-				enable_if_same<const _recordType&, typename detail::TypeId<_signature...>::HEAD > *_)
-		{
-			return ConstructorBuilder<_recordType, _signature...>(pNamespace, pRecord, FunctorType::CopyCtorConst);
-		}
-
-
-		template<class _recordType, class ..._signature>
-		inline constexpr const ConstructorBuilder<_recordType, _signature...>
-			ConstructorBuilder<>::select(const std::string& pNamespace, const std::string& pRecord,
-				enable_if_not_same<_recordType&, typename detail::TypeId<_signature...>::HEAD > *_,
-				enable_if_not_same<const _recordType&, typename detail::TypeId<_signature...>::HEAD > *__)
-		{
-			return ConstructorBuilder<_recordType, _signature...>(pNamespace, pRecord, FunctorType::Ctor);
-		}
-	}
 }
