f522615e0ae1cff4d52dde84edaeb5d6b4f51687
Neeraj <neeraj.singh31285@gmail.com>
2025-04-23 15:55:18 +0530

    Update Function.hpp

--- ReflectionTemplateLib/access/inc/Function.hpp
+++ ReflectionTemplateLib/access/inc/Function.hpp
@@ -9,11 +9,11 @@ namespace rtl {
 
     namespace access
     {
-		template<class ..._signature>
-		inline const FunctionCaller<_signature...> Function::bind() const
-		{
-			return FunctionCaller<_signature...>(*this);
-		}
+	template<class ..._signature>
+	inline const FunctionCaller<_signature...> Function::bind() const
+	{
+		return FunctionCaller<_signature...>(*this);
+	}
 
     /*  @method: hasSignature<...>()
         @param: set of arguments, explicitly specified as template parameter.
@@ -21,22 +21,22 @@ namespace rtl {
         * a single 'Function' object can be associated with multiple overloads of same function.
         * the set of arguments passed is checked agains all registered overloads, returns true if matched with any one.
     */  template<class ..._args>
-		inline const bool Function::hasSignature() const
-		{
-			//hasSignatureId() returns the index of the 'lambda' in functor-container, which cannot be '-1'.
-			return (hasSignatureId(detail::FunctorContainer<_args...>::getContainerId()) != -1);
-		}
+	inline const bool Function::hasSignature() const
+	{
+		//hasSignatureId() returns the index of the 'lambda' in functor-container, which cannot be '-1'.
+		return (hasSignatureId(detail::FunctorContainer<_args...>::getContainerId()) != -1);
+	}
 
 
     /*  @method: operator()()
         @param: variadic arguments.
         @return: RStatus, containing the call status & return value of from the reflected call.
         * if the arguments did not match with any overload, returns RStatus with Error::SignatureMismatch
-		* providing optional syntax, Function::call() does the exact same thing.
+	* providing optional syntax, Function::call() does the exact same thing.
     */  template<class ..._args>
-		inline RStatus Function::operator()(_args&& ...params) const noexcept
-		{
-			return bind().call(std::forward<_args>(params)...);
-		}
+	inline RStatus Function::operator()(_args&& ...params) const noexcept
+	{
+		return bind().call(std::forward<_args>(params)...);
 	}
+    }
 }
