40127b945cb07f71e987c7fd52c254eff6fadb0b
neeraj <neeraj.singh31285@gmail.com>
2025-05-23 16:47:59 +0530

    test cases added

--- ReflectionTemplateLibUnitTests/src/RObjectUnitTests.cpp
+++ ReflectionTemplateLibUnitTests/src/RObjectUnitTests.cpp
@@ -50,5 +50,89 @@ namespace rtl
             //Check if the value contained is same as given initially.
             ASSERT_EQ(str_cref, STRING_CHAR_POINTER);
         }
+
+
+        TEST(RObjectStringTests, init_with_ConstCharArray_view_as_StdString)
+        {
+            //Create an RObject that reflects a string value (init with const char*).
+            RObject robj = RObject::reflect<alloc::Stack>(STRING_CHAR_ARRAY);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'std::string'.
+            ASSERT_TRUE(robj.isReflecting<std::string>());
+
+            //get the view as type 'std::string'.
+            auto view = robj.view<std::string>();
+            ASSERT_TRUE(view.has_value());
+
+            const std::string& str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(str_cref, STRING_CHAR_ARRAY);
+        }
+
+
+        TEST(RObjectStringTests, init_with_ConstCharArray_view_as_StdStringView)
+        {
+            //Create an RObject that reflects a string value (init with const char*).
+            RObject robj = RObject::reflect<alloc::Stack>(STRING_CHAR_ARRAY);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'std::string_view'.
+            ASSERT_TRUE(robj.isReflecting<std::string_view>());
+
+            //get the view as type 'std::string_view'.
+            auto view = robj.view<std::string_view>();
+            ASSERT_TRUE(view.has_value());
+
+            const std::string_view& str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(str_cref, STRING_CHAR_ARRAY);
+        }
+
+
+        TEST(RObjectStringTests, init_with_StdString_view_as_StdString)
+        {
+            //Create an RObject that reflects a string value (init with const char*).
+            RObject robj = RObject::reflect<alloc::Stack>(STRING_STD_STRING);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'std::string'.
+            ASSERT_TRUE(robj.isReflecting<std::string>());
+
+            //get the view as type 'std::string'.
+            auto view = robj.view<std::string>();
+            ASSERT_TRUE(view.has_value());
+
+            const std::string& str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(str_cref, STRING_STD_STRING);
+        }
+
+
+        TEST(RObjectStringTests, init_with_StdString_view_as_StdStringView)
+        {
+            //Create an RObject that reflects a string value (init with const char*).
+            RObject robj = RObject::reflect<alloc::Stack>(STRING_STD_STRING);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'std::string_view'.
+            ASSERT_TRUE(robj.isReflecting<std::string_view>());
+
+            //get the view as type 'std::string_view'.
+            auto view = robj.view<std::string_view>();
+            ASSERT_TRUE(view.has_value());
+
+            const std::string_view& str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(str_cref, STRING_STD_STRING);
+        }
     }
 }
