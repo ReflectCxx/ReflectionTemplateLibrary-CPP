0df9acc3a51a15295f276953a6de6265190ea90e
neeraj31285 <neeraj.singh31285@gmail.com>
2025-05-23 19:47:34 +0530

    few more testcases added

--- ReflectionTemplateLibUnitTests/inc/RObjectUnitTests.h
+++ ReflectionTemplateLibUnitTests/inc/RObjectUnitTests.h
@@ -7,7 +7,9 @@ namespace rtl
 	namespace unit_test
 	{
 		static const std::string STRING_STD_STRING = "string_type: std::string.";
-		static constexpr const char* STRING_CHAR_POINTER = "string_type: const_char_*.";
-		static constexpr const char STRING_CHAR_ARRAY[] = "string_type: const_char_array.";
+		static constexpr const char* STRING_CONST_CHAR_POINTER = "string_type: const_char_*.";
+
+		static char STRING_CHAR_ARRAY[] = "string_type: const_char_array.";
+		static constexpr const char STRING_CONST_CHAR_ARRAY[] = "string_type: const_char_array.";
 	}
 }
--- ReflectionTemplateLibUnitTests/src/RObjectUnitTests.cpp
+++ ReflectionTemplateLibUnitTests/src/RObjectUnitTests.cpp
@@ -10,10 +10,10 @@ namespace rtl
 {
     namespace unit_test
     {
-        TEST(RObjectStringTests, init_with_ConstCharPtr_view_as_StdString)
+        TEST(RObjectStringTests, init_with_constCharPtr_view_as_stdString)
         {
-            //Create an RObject that reflects a string value (init with const char*).
-            RObject robj = RObject::reflect(STRING_CHAR_POINTER);
+            //Create an RObject that reflects a string value (init with 'const char*').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_POINTER);
 
             //check if the RObject is reflecting anything?
             ASSERT_TRUE(robj.isReflecting());
@@ -27,14 +27,14 @@ namespace rtl
 
             const std::string& str_cref = view->get();
             //Check if the value contained is same as given initially.
-            ASSERT_EQ(str_cref, STRING_CHAR_POINTER);
+            ASSERT_EQ(str_cref, STRING_CONST_CHAR_POINTER);
         }
 
 
-        TEST(RObjectStringTests, init_with_ConstCharPtr_view_as_StdStringView)
+        TEST(RObjectStringTests, init_with_constCharPtr_view_as_stdStringView)
         {
-            //Create an RObject that reflects a string value (init with const char*).
-            RObject robj = RObject::reflect(STRING_CHAR_POINTER);
+            //Create an RObject that reflects a string value (init with 'const char*').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_POINTER);
 
             //check if the RObject is reflecting anything?
             ASSERT_TRUE(robj.isReflecting());
@@ -48,14 +48,35 @@ namespace rtl
 
             const std::string_view& str_cref = view->get();
             //Check if the value contained is same as given initially.
-            ASSERT_EQ(str_cref, STRING_CHAR_POINTER);
+            ASSERT_EQ(str_cref, STRING_CONST_CHAR_POINTER);
         }
 
 
-        TEST(RObjectStringTests, init_with_ConstCharArray_view_as_StdString)
+        TEST(RObjectStringTests, init_with_constCharPtr_view_as_constCharPtr)
         {
-            //Create an RObject that reflects a string value (init with const char*).
-            RObject robj = RObject::reflect(STRING_CHAR_ARRAY);
+            //Create an RObject that reflects a string value (init with 'const char*').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_POINTER);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'const char*'.
+            ASSERT_TRUE(robj.isReflecting<const char*>());
+
+            //get the view as type 'const char*'.
+            auto view = robj.view<const char*>();
+            ASSERT_TRUE(view.has_value());
+
+            const char* str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(std::string(str_cref), STRING_CONST_CHAR_POINTER);
+        }
+
+
+        TEST(RObjectStringTests, init_with_constCharArray_view_as_stdString)
+        {
+            //Create an RObject that reflects a string value (init with 'const char[]').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_ARRAY);
 
             //check if the RObject is reflecting anything?
             ASSERT_TRUE(robj.isReflecting());
@@ -69,13 +90,55 @@ namespace rtl
 
             const std::string& str_cref = view->get();
             //Check if the value contained is same as given initially.
-            ASSERT_EQ(str_cref, STRING_CHAR_ARRAY);
+            ASSERT_EQ(str_cref, STRING_CONST_CHAR_ARRAY);
+        }
+
+
+        TEST(RObjectStringTests, init_with_constCharArray_view_as_stdStringView)
+        {
+            //Create an RObject that reflects a string value (init with 'const char[]').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_ARRAY);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'std::string_view'.
+            ASSERT_TRUE(robj.isReflecting<std::string_view>());
+
+            //get the view as type 'std::string_view'.
+            auto view = robj.view<std::string_view>();
+            ASSERT_TRUE(view.has_value());
+
+            const std::string_view& str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(str_cref, STRING_CONST_CHAR_ARRAY);
+        }
+
+
+        TEST(RObjectStringTests, init_with_constCharArray_view_as_constCharPtr)
+        {
+            //Create an RObject that reflects a string value (init with 'const char[]').
+            RObject robj = RObject::reflect(STRING_CONST_CHAR_ARRAY);
+
+            //check if the RObject is reflecting anything?
+            ASSERT_TRUE(robj.isReflecting());
+
+            //check if the RObject is reflecting value type 'const char*'.
+            ASSERT_TRUE(robj.isReflecting<const char*>());
+
+            //get the view as type 'const char*'.
+            auto view = robj.view<const char*>();
+            ASSERT_TRUE(view.has_value());
+
+            const char* str_cref = view->get();
+            //Check if the value contained is same as given initially.
+            ASSERT_EQ(std::string(str_cref), STRING_CONST_CHAR_ARRAY);
         }
 
 
         TEST(RObjectStringTests, init_with_stdString_view_as_stdString)
         {
-            //Create an RObject that reflects a string value (init with const char*).
+            //Create an RObject that reflects a string value (init with std::string).
             RObject robj = RObject::reflect(STRING_STD_STRING);
 
             //check if the RObject is reflecting anything?
@@ -96,7 +159,7 @@ namespace rtl
 
         TEST(RObjectStringTests, init_with_stdString_view_as_stdStringView)
         {
-            //Create an RObject that reflects a string value (init with const char*).
+            //Create an RObject that reflects a string value (init with std::string).
             RObject robj = RObject::reflect(STRING_STD_STRING);
 
             //check if the RObject is reflecting anything?
@@ -117,22 +180,30 @@ namespace rtl
 
         TEST(RObjectStringTests, init_with_stdString_view_as_constCharPtr)
         {
-            //Create an RObject that reflects a string value (init with const char*).
-            RObject robj = RObject::reflect(STRING_CHAR_ARRAY);
+            //Create an RObject that reflects a string value (init with std::string).
+            RObject robj = RObject::reflect(STRING_STD_STRING);
 
             //check if the RObject is reflecting anything?
             ASSERT_TRUE(robj.isReflecting());
 
-            //check if the RObject is reflecting value type 'std::string_view'.
+            //check if the RObject is reflecting value type 'const char*'.
             ASSERT_TRUE(robj.isReflecting<const char*>());
 
-            //get the view as type 'std::string_view'.
+            //get the view as type 'const char*'.
             auto view = robj.view<const char*>();
             ASSERT_TRUE(view.has_value());
 
             const char* str_cref = view->get();
             //Check if the value contained is same as given initially.
-            ASSERT_EQ(std::string(str_cref), STRING_CHAR_ARRAY);
+            ASSERT_EQ(std::string(str_cref), STRING_STD_STRING);
         }
+
+    /*  this won't compile since 'char[]' is not const. treated as unvalid string.
+        TEST(RObjectStringTests, init_with_charArray_view_as_stdString)
+        {
+            //Create an RObject that reflects a string value (init with  char[]).
+            RObject robj = RObject::reflect(STRING_CHAR_ARRAY);
+        }
+    */
     }
 }
