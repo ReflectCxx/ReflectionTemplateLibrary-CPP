cbbecb6d9d0c26df14d71bc10fb2e62798defe02
neeraj31285 <neeraj.singh31285@gmail.com>
2025-08-18 10:30:56 +0530

    formatted license header.

--- ReflectionTemplateLib/detail/src/ReflectCast.cpp
+++ ReflectionTemplateLib/detail/src/ReflectCast.cpp
@@ -1,11 +1,12 @@
-/*****************************************************************
- *                                                               *
- *  https://github.com/ReflectCxx/ReflectionTemplateLibrary-CPP  *
- *                                                               *
- *  Copyright (c) 2025 Neeraj Singh (reflectcxx@outlook.com)     *
- *  SPDX-License-Identifier: MIT                                 *
- *                                                               *
- *****************************************************************/
+/*************************************************************************
+ *                                                                       *
+ *  Reflection Template Library (RTL) - Modern C++ Reflection Framework  *
+ *  https://github.com/ReflectCxx/ReflectionTemplateLibrary-CPP          *
+ *                                                                       *
+ *  Copyright (c) 2025 Neeraj Singh <reflectcxx@outlook.com>             *
+ *  SPDX-License-Identifier: MIT                                         *
+ *                                                                       *
+ *************************************************************************/
 
 
 #include "ReflectCast.hpp"
