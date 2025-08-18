9dabab8e6390a7d2064b9f40d2ca33e72541cbc3
neeraj31285 <neeraj.singh31285@gmail.com>
2025-08-18 09:54:00 +0530

    formatted license header.

--- ReflectionTemplateLib/common/error_codes.h
+++ ReflectionTemplateLib/common/error_codes.h
@@ -1,6 +1,7 @@
-/*/*****************************************************************
+/*****************************************************************
  *                                                               *
  *  https://github.com/ReflectCxx/ReflectionTemplateLibrary-CPP  *
+ *                                                               *
  *  Copyright (c) 2025 Neeraj Singh (reflectcxx@outlook.com)     *
  *  SPDX-License-Identifier: MIT                                 *
  *                                                               *
